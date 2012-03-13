bd3f92f5e7cf409b7bde6e2e29bbbbe968ed55ec
Pavan Keerthi <pavan.keerthi@3dmxconsulting.co.uk>
2012-03-13 17:01:04 +0000

    Completed Workflow StoredProc

--- SQL2008R2/SQLDB/Create Procedures.sql
+++ SQL2008R2/SQLDB/Create Procedures.sql
@@ -6,14 +6,17 @@ Change Log:
 -----------
 07-March-2012 :Created Initial Version.
 *********************************************************************************************/
-/* Create a procedure to insert data into ETL_Workflow Table via a table-valued parameter(TVP) */
+/* Create a procedure to insert data into ETL_Workflow Table via a table-valued parameter(TVP)
+and return Workflow Id */
 CREATE PROCEDURE usp_InsertWorkflowInfo
-	(@WorkflowInfo WorkflowTableType READONLY)
+	(@WorkflowInfo WorkflowTableType READONLY,@WorkflowId int OUTPUT)
 AS
 INSERT INTO [ETLFramework2008].[dbo].[ETL_Workflow]
-           ([Workflow_Name]
-           ,[Workflow_Status]
-           ,[WorkFlow_RecoveryMode])
+           ([Workflow_Name],[Workflow_Status],[WorkFlow_RecoveryMode])
+Select WorkflowName,WorkflowStatus,WorkflowRecoveryMode  
+FROM @WorkflowInfo 
+
+Select @WorkflowId=SCOPE_IDENTITY()
 
 GO
 
